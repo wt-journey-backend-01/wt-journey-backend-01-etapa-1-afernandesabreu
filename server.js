e872a359ece280b542dd89a377ee22856abd9b20
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-10 17:33:45 -0300

    corrigindo erro de digitação -ln11

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views, index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
 app.get(/(.*)/, (req, res) => {
