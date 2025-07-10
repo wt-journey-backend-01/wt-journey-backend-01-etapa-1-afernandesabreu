a0b6e2057aa27bcb18f055dfd4f3b159f8756152
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-10 14:05:30 -0300

    adicionando código inicial para server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views, index.html'));
+})
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
