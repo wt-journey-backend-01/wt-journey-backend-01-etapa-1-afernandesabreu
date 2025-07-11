023af269e2c17a50142ea4e99bdc7af9ce34d57b
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-11 00:52:51 -0300

    remoção de middleware estático e configuração de rota /sugestao com método GET

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/sugestao', (req, res) => {
+  const {nome, ingredientes } = req.query
+  return res.send(`
+    <h1>Sugestão recebida</h1>
+    <p>Obrigado ${nome}!</p>
+    <p>Sua sugestão de ${ingredientes} foi salva com sucesso!</p>`)
+});
 
 app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
