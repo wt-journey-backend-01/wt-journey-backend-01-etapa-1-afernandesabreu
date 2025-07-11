a33db09ebbe42bd0192cc83513faedb9ae9d2527
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-11 18:36:13 -0300

    adicionando âncora para rota / na rota /sugestao

--- server.js
+++ server.js
@@ -12,10 +12,24 @@ app.get('/', (req, res) => {
 
 app.get('/sugestao', (req, res) => {
   const {nome, ingredientes } = req.query
-  return res.send(`
-    <h1>Sugestão recebida</h1>
-    <p>Obrigado ${nome}!</p>
-    <p>Sua sugestão de ${ingredientes} foi salva com sucesso!</p>`)
+  return res.send(
+    `
+    <!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Agradecemos sua sugestão</title>
+      </head>
+      <body>
+        <h1>Sugestão recebida</h1>
+        <p>Obrigado, ${nome}!</p>
+        <p>Sua sugestão de ${ingredientes} foi salva com sucesso.</p>
+        <a href="/">Voltar</a>
+      </body>
+    </html>
+    `
+  );
 });
 
 app.get(/(.*)/, (req, res) => {
