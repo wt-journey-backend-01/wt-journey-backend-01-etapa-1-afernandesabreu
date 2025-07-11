3f9adeba7c1ef817ab25e1c0899ee30579e91539
afernandesabreu <178678281+afernandesabreu@users.noreply.github.com>
2025-07-11 19:26:00 -0300

    adicionando rota get /contato

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/contato', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
 app.get('/sugestao', (req, res) => {
   const {nome, ingredientes } = req.query
   return res.send(
